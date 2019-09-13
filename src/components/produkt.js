9cb838055ebc1ea64c612578fdb81ce70c6050a5
rsoriano <rsorianosanchis@gmail.com>
2019-09-13 15:46:43 +0200

    v. 2019/09/13--15:46

--- src/components/produkt.js
+++ src/components/produkt.js
@@ -4,12 +4,15 @@ import axios from "axios";
 
 const Produkt = () => {
      const [loading, setLoading] = useState(false);
+     
 
      useEffect(() => {
+        
        const getCockpitProdukterna = async () => {
+        
          setLoading(true);
          const result = await axios.get(
-           "http://localhost:8080/api/collections/get/produkterna"
+           `http://localhost:8080/api/collections/get/produkterna?filter[_id]=5d774d7c666138002d000133`
          );
          console.log(result);
          //storeArticles(result.data.entries);
@@ -27,3 +30,42 @@ const Produkt = () => {
 };
 
 export default Produkt;
+
+
+
+/*********************************/
+
+// const Produkt = () => {
+//      const [loading, setLoading] = useState(false);
+//      const [produktId, setProduktId] = useState('');
+//         setProduktId(()=>{
+//             return window.location.pathname.split("/")[2];
+//         });
+
+//      useEffect(() => {
+        
+//        const getCockpitProdukterna = async () => {
+//         if(window.location.pathname.split("/")[2])
+//          setLoading(true);
+//          const result = await axios.get(
+//            `http://localhost:8080/api/collections/get/produkterna?filter[_id]=${produktId}`
+//          );
+//          console.log(result);
+//          //storeArticles(result.data.entries);
+//          setLoading(false);
+//        };
+//        getCockpitProdukterna();
+//      }, [produktId]);
+
+//   return (
+//     <Fragment>
+      
+//       <h1 className="text-center">Produktens detalj</h1>
+//     </Fragment>
+//   );
+// };
+
+// export default Produkt;
+
+// //  `http://localhost:8080/api/collections/get/produkterna?filter[_id]=${produktId}`
+// window.location.pathname.split("/")[2];
